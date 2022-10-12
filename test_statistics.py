ef7cab9a5b7a0bf778a904086b023c3e6e4b8973
Pinpimarn Pinpisit <pinpimarn.p@ku.th>
2022-10-12 14:26:27 +0700

    fix

--- test_statistics.py
+++ test_statistics.py
@@ -1,19 +1,31 @@
 from unittest import TestCase
-from statistics import variance
+from statistics import variance, stdev
+from math import sqrt
 
 
 class StatisticsTest(TestCase):
 
+    def setUp(self):
+        self.data1 = [10.0, 10.0, 10.0, 10.0, 10.0]
+        self.data2 = [1, 2, 3, 4, 5]
+        self.data3 = [10, 2, 8, 4 ,6]
+
     def test_variance_typical_values(self):
         """variance of typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
+        self.assertEqual(0.0, variance(self.data1))
+        self.assertEqual(2.0, variance(self.data2))
+        self.assertEqual(8.0, variance(self.data3))
 
     def test_variance_throws_exception(self):
         """variance of an empty list should raise an exception"""
         with self.assertRaises(ValueError):
             var = variance([])
+
+    def test_stdev(self):
+        """standard deviation of typical values"""
+        self.assertEqual(0.0, stdev(self.data1))
+        self.assertEqual(sqrt(2.0), stdev(self.data2))
+        self.assertEqual(sqrt(8.0), stdev(self.data3))
 
 
 if __name__ == '__main__':
